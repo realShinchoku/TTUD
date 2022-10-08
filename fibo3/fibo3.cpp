66c78ca1e8a779eaf682db341469f7de8e800a11
Shinchoku <tienmp3@gmail.com>
2022-10-08 08:01:45 +0700

    f

--- fibo3/fibo3.cpp
+++ fibo3/fibo3.cpp
@@ -8,13 +8,14 @@ int main()
     int n;
     cout << "Nhap n = ";
     cin >> n;
-    vector<long long> fibo(n+6);
-    fibo[0] = 0;fibo[1] = 1;fibo[2] = 2;
+    vector<long long> fibo;
+    fibo.push_back(0);fibo.push_back(1);fibo.push_back(2);
     for (int i = 3; i <= n; i+=3){
-        fibo[i] = fibo[i/3 *2];
-        fibo[i+1] = fibo[i/3 *2] + fibo[i/3 *2 +1];
-        fibo[i+2] = fibo[i/3 *2] + fibo[i/3 *2 +1] + fibo[i/3*2 +2];
+        int k = i*0.6666666666666666666666666666666666667;
+        fibo.push_back(fibo[k]);
+        fibo.push_back(fibo[k] + fibo[k+1]);
+        fibo.push_back(fibo[k] + fibo[k+1] + fibo[k+2]);
     }
-    cout << "f("<<n<<") = " << fibo[n];
+    cout << "f("<<n<<") = " << fibo[n] << endl;
     return 0;
 }
