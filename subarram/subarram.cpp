c91b2767e6d2647577e0b03003aa0822aca1a80f
Shinchoku <tienmp3@gmail.com>
2022-10-08 10:43:12 +0700

    reaname

---
+++ subarram/subarram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cout << "Nhap m = "; cin >> m;
+    cout << "Nhap n = "; cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cout << "a["<<i<<"] = ";
+        cin >> a[i];
+    }
+    cout << "Co tat ca ";
+}
