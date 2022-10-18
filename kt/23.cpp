5116f86bf7f3ba86b44f8f35d83545df8aeca482
Shinchoku <tienmp3@gmail.com>
2022-10-18 08:56:00 +0700

    kt2

--- kt/23.cpp
+++ kt/23.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <stack>
+#include <queue>
+#include <string>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    int c[n];
+    stack<int> a;
+    queue<int> b;
+    for(int i = 0; i < n; i ++){
+        cin >> c[i];
+    }
+    for(int i = n-1; i >=0; i--){
+        a.push(c[i]);
+    }
+    string s;
+    cin.ignore();
+    getline(cin,s);
+    for(int i = 0; i < s.size(); i ++){
+        if(s[i]=='C'){
+            if(a.size()>0){
+                b.push(a.top());
+                a.pop();
+            }
+        }
+        else if (s[i] == 'H')
+        {
+           if(b.size()>0){
+                a.push(b.front());
+                b.pop();
+            }
+        }
+    }
+    while (a.size())
+    {
+        cout << a.top();
+        if(a.size() > 1)
+            cout << " ";
+        a.pop();
+    }
     return 0;
 }
