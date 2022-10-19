a72b82d19cb609e7aa3143c82af3163f8e4bf354
Shinchoku <tienmp3@gmail.com>
2022-10-20 06:38:42 +0700

    btl

--- BTL/vd1.cpp
+++ BTL/vd1.cpp
@@ -7,7 +7,8 @@ int n, S, cnt, cnt_best, sum, t[maxn], mark[maxn], mark_best[maxn], t_max[maxn];
 
 void enter()
 {
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n >> S;
     for (int i = 1; i <= n; ++i)
         cin >> t[i];
@@ -18,7 +19,7 @@ void create_data()
     // cnt_best là số tờ tiền sử dụng trong phương án tốt nhất.
     // Ban đầu chưa có phương án nào, gán cnt_best = n + 1.
     cnt_best = n + 1;
-	
+
     // t_max[i] lưu giá trị của tờ tiền lớn nhất từ i tới n.
     t_max[n] = t[n];
     for (int i = n - 1; i >= 0; --i)
@@ -31,7 +32,7 @@ void update_best_solution()
     if (sum == S && cnt < cnt_best)
     {
         cnt_best = cnt;
-		
+
         for (int i = 1; i <= n; ++i)
             mark_best[i] = mark[i];
     }
@@ -46,7 +47,7 @@ void printf_result()
     else // Tìm được thì in ra cách trả đó.
     {
         cout << cnt_best << endl;
-		
+
         for (int i = 1; i <= n; ++i)
             if (mark_best[i])
                 cout << t[i] << ' ';
@@ -56,23 +57,24 @@ void printf_result()
 void branch_and_bound(int i)
 {
     // Nếu nghiệm mở rộng của nhánh này không tốt hơn thì return.
-    if (cnt + (S - sum) / t_max[i + 1] >= cnt_best)
-        return;
-		
+    if (t_max[i + 1] != 0)
+        if (cnt + (S - sum) / t_max[i + 1] >= cnt_best)
+            return;
+
     for (int j = 0; j <= 1; ++j)
     {
         // Ghi nhận thành phần thứ i.
-        sum = sum + t[i] * j; 
+        sum = sum + t[i] * j;
         mark[i] = j;
         cnt += j;
-		
-        if (i == n) 
+
+        if (i == n)
             update_best_solution();
-        else if (sum <= S) 
+        else if (sum <= S)
             branch_and_bound(i + 1);
-			
+
         // Loại bỏ thành phần thứ i.
-        sum -= t[i] * j; 
+        sum -= t[i] * j;
         cnt -= j;
     }
 }
